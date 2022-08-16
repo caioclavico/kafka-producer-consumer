f02c10287bb140fcc1f2056ed7a4c7025ce1965b
Ismael Correia <ismaelavilacorreia@gmail.com>
2022-08-16 14:21:57 -0300

    feat: consumer

--- src/kafka_producer_consumer/consumer.clj
+++ src/kafka_producer_consumer/consumer.clj
@@ -1,6 +1,38 @@
-(ns kafka-producer-consumer.consumer)
+(ns kafka-producer-consumer.consumer
+  (:gen-class)
+  (:require
+    [clojure.data.json :as json]
+    [clojure.java.io :as jio])
+  (:import
+    (org.apache.kafka.clients.consumer ConsumerConfig KafkaConsumer)
+    (java.time Duration)
+    (java.util Properties)))
+
+(defn- build-props [fname]
+  (with-open [config (jio/reader fname)]
+    (doto (Properties.)
+      (.putAll {ConsumerConfig/GROUP_ID_CONFIG                 "clojure_example_group"
+                ConsumerConfig/KEY_DESERIALIZER_CLASS_CONFIG   "org.apache.kafka.common.serialization.StringDeserializer"
+                ConsumerConfig/VALUE_DESERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringDeserializer"})
+      (.load config))))
+
+(defn consumer! [fname topic]
+  (with-open [consumer (KafkaConsumer. (build-props fname))]
+    (.subscribe consumer [topic])
+    (loop [tc 0 records []]
+      (let [new-tc (reduce
+          (fn [tc record]
+            (let [value (.value record)
+              count (get (json/read-str value) "count")
+              new-tc (+ tc cnt)]
+              (printfn "Consumed key %s, value %s, total count is %d\n" (.key record) value new-tc)
+              new-tc))
+            tc records)]
+      (println "Waiting for message")
+      (recur new-tc
+        (sec (.poll consumer (Duration/ofSeconds 1))))))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "I kind of do something ... maybe."
   [& args]
-  (println "Hello, consumer!"))
+  (apply consume args))
